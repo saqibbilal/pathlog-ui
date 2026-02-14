0c056c0576f4ef84e08fcd73dc698b0de4037d77
Saqib Bilal <saqib_bilal786@yahoo.com>
2026-02-13 23:32:17 -0500

    chore: initial React + Vite + TS + Tailwind v4 setup

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+    return (
+        <div className="flex h-screen items-center justify-center bg-blue-50">
+            <h1 className="text-4xl font-bold text-blue-600 underline decoration-wavy">
+                PathLog is Live!
+            </h1>
+        </div>
+    )
+}
+
+export default App
